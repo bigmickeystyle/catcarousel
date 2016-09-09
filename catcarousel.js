9ef4934e39e202b5ef18c746fc46472c330b6d52
bigmickeystyle <mike.123.smith@gmail.com>
2016-09-09 13:47:30 +0200

    working

--- catcarousel.js
+++ catcarousel.js
@@ -1,33 +1,55 @@
 (function(){
+    var clearer;
+    var timer;
     var cats = document.getElementsByClassName('cat');
     var buttons = document.getElementsByClassName('button');
-    var i;
-    function movecat(){
-        for (i = 0; i < cats.length; i++){
-            (function(i){
-                setTimeout(function(){
-                    cats[i].className = "cat moving2";
-                    buttons[i].className = "button";
-                    if (i == 3) {
-                        cats[i - 1].className = "cat";
-                        cats[i - 3].className = "cat moving";
-                        buttons[i - 3].className = "button whitebutton";
-                        movecat();
-                    }
+    var i = 0;
 
-                    else {
-                        cats[i + 1].className = "cat moving";
-                        buttons[i + 1].className = "button whitebutton";
-                        if (i == 0) {
-                            cats[i + 3].className = "cat";
-                        }
-                        else {
-                            cats[i - 1].className = "cat";
-                        }
-                    }
-                }, 1000 * (i + 1));
-            })(i);
+    var catReset = function(){
+        for (var l = 0; l < cats.length; l++){
+            if (cats[l].classList.value == ("cat gomiddle goleft")){
+                cats[l].classList.remove("gomiddle", "goleft");
+            }
         }
+    };
+
+
+
+
+    function movecat(){
+        // shift the cats
+        cats[i % 4].classList.add("goleft");
+        cats[(i + 1) % 4].classList.add("gomiddle");
+
+        //shift the buttons
+        buttons[i % 4].className = "button";
+        buttons[(i + 1) % 4].className = "button whitebutton";
+
+        // shift all cats to the right after half a second
+
+        clearer = setTimeout(catReset, 500);
+
+        i++;
+
+        for (var j = 0; j < buttons.length; j++){
+            buttons[j].addEventListener('click', function(){
+                clearTimeout(timer);
+                clearTimeout(clearer);
+                var pressed = parseInt(this.id);
+                for (var k = 0; k < cats.length; k++){
+                    if (cats[k].classList.value == ("cat gomiddle")){
+                        cats[k].classList.add("goleft");
+                    }
+                    buttons[k].className = "button";
+                }
+                cats[pressed].className ="cat gomiddle";
+                buttons[pressed].className = "button whitebutton";
+                i = pressed;
+                timer = setTimeout(movecat, 2000);
+                setTimeout(catReset, 500);
+            });
+        }
+        timer = setTimeout(movecat, 5000);
     }
     movecat();
 })();
