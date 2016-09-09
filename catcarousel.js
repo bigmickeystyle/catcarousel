d87190be850ce02ddb46d57f534c1d79f3759e1f
bigmickeystyle <mike.123.smith@gmail.com>
2016-09-09 17:02:49 +0200

    tentative swipe left

--- catcarousel.js
+++ catcarousel.js
@@ -2,28 +2,28 @@
     var clearer;
     var timer;
     var cats = document.getElementsByClassName('cat');
+    var len = cats.length;
     var buttons = document.getElementsByClassName('button');
     var i = 0;
+    var start;
+    var end;
 
     var catReset = function(){
-        for (var l = 0; l < cats.length; l++){
+        for (var l = 0; l < len; l++){
             if (cats[l].classList.value == ("cat gomiddle goleft")){
                 cats[l].classList.remove("gomiddle", "goleft");
             }
         }
     };
 
-
-
-
     function movecat(){
         // shift the cats
-        cats[i % 4].classList.add("goleft");
-        cats[(i + 1) % 4].classList.add("gomiddle");
+        cats[i % len].classList.add("goleft");
+        cats[(i + 1) % len].classList.add("gomiddle");
 
         //shift the buttons
-        buttons[i % 4].className = "button";
-        buttons[(i + 1) % 4].className = "button whitebutton";
+        buttons[i % len].className = "button";
+        buttons[(i + 1) % len].className = "button whitebutton";
 
         // shift all cats to the right after half a second
 
@@ -31,25 +31,57 @@
 
         i++;
 
-        for (var j = 0; j < buttons.length; j++){
-            buttons[j].addEventListener('click', function(){
-                clearTimeout(timer);
-                clearTimeout(clearer);
-                var pressed = parseInt(this.id);
-                for (var k = 0; k < cats.length; k++){
-                    if (cats[k].classList.value == ("cat gomiddle")){
-                        cats[k].classList.add("goleft");
-                    }
-                    buttons[k].className = "button";
-                }
-                cats[pressed].className ="cat gomiddle";
-                buttons[pressed].className = "button whitebutton";
-                i = pressed;
-                timer = setTimeout(movecat, 2000);
-                setTimeout(catReset, 500);
-            });
-        }
+        console.log("normal");
         timer = setTimeout(movecat, 5000);
     }
-    movecat();
+
+    for (var j = 0; j < buttons.length; j++){
+        buttons[j].addEventListener('click', function(){
+            clearTimeout(timer);
+            clearTimeout(clearer);
+            var pressed = parseInt(this.id);
+            for (var k = 0; k < cats.length; k++){
+                if (cats[k].classList.value == ("cat gomiddle")){
+                    cats[k].classList.add("goleft");
+                }
+                buttons[k].className = "button";
+            }
+            cats[pressed].className ="cat gomiddle";
+            buttons[pressed].className = "button whitebutton";
+            i = pressed;
+            console.log("button");
+            timer = setTimeout(movecat, 5000);
+            setTimeout(catReset, 500);
+        });
+        buttons[j].addEventListener('mouseover', function(){
+            this.style.cursor = 'pointer';
+        });
+    }
+    swipe();
+    console.log ("start");
+    setTimeout(movecat, 5000);
+
+    function swipe(){
+
+        document.body.addEventListener('touchstart', function(e){
+            e.preventDefault();
+            start = e.touches[0].clientX;
+        });
+
+        document.body.addEventListener('touchend', function(e){
+            e.preventDefault();
+            end = e.changedTouches[0].clientX;
+            if (start > end){
+                console.log("go!");
+                clearTimeout(timer);
+                clearTimeout(clearer);
+                movecat();
+            }
+            start = null;
+            end = null;
+        });
+
+
+    }
+
 })();
